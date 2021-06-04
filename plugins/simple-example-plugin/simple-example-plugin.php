f8176f48c76d397ce6c32cfaac99d13b97b32196
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2021-06-04 09:58:33 -0400

    spelt authour wrongly (not wrongly) in the plugin php file

--- plugins/simple-example-plugin/simple-example-plugin.php
+++ plugins/simple-example-plugin/simple-example-plugin.php
@@ -3,7 +3,7 @@
     Plugin Name: Simple Example Plugin
     Description: Welcome to WordPress Plugin Development
     Plugin URI: https://plugin-planet.com
-    Authour: Gilbert Rabut Tsurwa
+    Author: Gilbert Rabut Tsurwa
     Version: 1.0
     License: GPLv2 or later
     License URI: https://www.gnu.org/licenses/gpl-2.0.txt
