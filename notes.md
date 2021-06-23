6ac950566b97229ea85b7c38f7bc2c18cbdecff2
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2021-06-23 12:04:18 -0400

    Add Administration Menus - Part 1 - learnt how to add top-level and sub-level menus for our plugin - went through some important functions that make this possible - these functions, and more essential information can be found (as always) in the notes.md file

--- notes.md
+++ notes.md
@@ -1,3 +1,10 @@
-## Summary
-- created our plugin folder and file architecture
-- this is a good boilerplate/template for the making of future plugins
+[wp-add-menu-page-link]: https://developer.wordpress.org/reference/functions/add_menu_page/
+[wp-add-submenu-page-link]: https://developer.wordpress.org/reference/functions/add_submenu_page/
+[wp-admin-menu-page-link]: https://developer.wordpress.org/reference/hooks/admin_menu/
+## Summary 
+
+- used code to make our plugin appear on our WordPress admin as a top-level menu, and as a sublevel menu.
+- went through the important [add_menu_page()][wp-add-menu-page-link] and [add_submenu_page()][wp-add-submenu-page-link] respectively
+- unlike Jeff (the instructor), i included both the code to make our plugin appear on the menu *and* the submenu
+- both sets of code (that are within functions) use the  myplugin_display_settings_page(), which can be found [here](plugins/myplugin/myplugin.php)
+- lastly, but of more minour importance, i got exposed to the [admin_menu][wp-admin-menu-page-link] WordPress hook for the first time
--- plugins/myplugin/myplugin.php
+++ plugins/myplugin/myplugin.php
@@ -12,14 +12,70 @@
     License URI: https://www.gnu.org/licenses/gpl-2.0.txt
 */
 
-/**
- * NOTE:
- * the code below prevent direct access to our php files, for security
- * it should appear before any other code, ideally right below the plugin metadata info (look on top)
- */
-
 
 // exit if the file is called directly
 if (!defined("ABSPATH")) {
     exit;
 }
+
+// NOTE: display the plugin settings page
+function myplugin_display_settings_page() {
+    // check if user is has permission
+    if (!current_user_can("manage_options")) return;
+    ?>
+
+    <div class="wrap">
+        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+        <!-- NOTE: form action must always be options.php -->
+        <!-- ... and form method must always be post -->
+        <form action="options.php" method="POST">
+            <?php
+                // output security fields
+                settings_fields("myplugin_options");
+                // output setting sections
+                do_settings_sections("myplugin");
+                // submit button
+                submit_button();
+            ?>
+        </form>
+    </div>
+            <?php
+}
+
+/**
+ * NOTE: this function adds a top-level administrative menu
+ */
+function myplugin_add_toplevel_menu() {
+    // IMPORTANT: hover over the add_menu_page() to get further details on the function's parametre
+    add_menu_page(
+        "MyPlugin Settings", 
+        "MyPlugin", 
+        "manage_options", 
+        "myplugin", 
+        "myplugin_display_settings_page", 
+        "dashicons-admin-generic", 
+        null
+    );
+}
+
+/**
+ * NOTE:
+ * whereas this one adds a sublevel menu (unlike Jeff, I'm including both in this file)
+ * IMPORTANT: the first parametre of the add_submenu_page() is the parent slug.
+ * in other words, it is the section in which the sublevel menu will be found
+ * options-general.php usually refers to the settings menu (i may explore the other values later)
+ * other than this, all the other parametres are the same ones as the ones used in the add_menu_page()
+ */
+function myplugin_add_sublevel_menu() {
+    add_submenu_page(
+        "options-general.php",
+        "MyPlugin Settings",
+        "MyPlugin",
+        "manage_options",
+        "myplugin",
+        "myplugin_display_settings_page"
+    );
+}
+
+add_action("admin_menu", "myplugin_add_toplevel_menu");
+add_action("admin_menu", "myplugin_add_sublevel_menu");
