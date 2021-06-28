47b55ba1dd4c8b34c6f9cb85b9cffcfc3124bd83
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2021-06-28 15:40:55 -0400

    Add the Plugin Settings Page - Part 1 - a lot was done in this commit. details in the notes.md file - to summarise, i am building the foundations on creating the markup and functionality of the plugin settings page - this is as shallow as i will get with my overview. i go on deeper detail on the notes markdown file

--- notes.md
+++ notes.md
@@ -1,12 +1,15 @@
-[php-require-once-link]: https://www.php.net/manual/en/function.require-once.php
-[wp-plugin-dir-path-link]: https://developer.wordpress.org/reference/functions/plugin_dir_path/
+[wp-settings-api]: https://developer.wordpress.org/plugins/settings/settings-api/
+[wp-add-settings-section-link]: https://developer.wordpress.org/reference/functions/add_settings_section/
+[wp-add-settings-field-link]: https://developer.wordpress.org/reference/functions/add_settings_field/
 ## Summary
-- separated the code from the last commit into different php files (organisation things)
-- we then "required" those files in our main plugin php file (where the code originally was) using [require_once][php-require-once-link] keyword and the [plugin_dir-path()][php-require-once-link]
-- this functionality is very similar to how *import* and *require* work in Javascript
-- in our case, the order in which we write the require code doesn't matter. are there any instances in which it does?
-- also, i deleted all of the comments (not including the native PHP ones, of course), as code is the exact same in the last commit
-  - to reference the code comments, go to the previous commit
+- **OK**, a lot was done in this commit, mainly involving the [Settings API][wp-settings-api]
+- all relevant code is written in the [myplugin.php](plugins/myplugin/myplugin.php) file
+- to attempt to summarise, i am using the ```myplugin_register_settings()``` to do the following:
+  - using the [```add_settings_section()```][wp-add-settings-section-link] to add section settings in the plugin page in the login screen and in the admin area
+  - using the [```add_settings_field()```][wp-add-settings-field-link] to add certain fields inside a section (in our case, the login screen section or the admin area section)
+- the list of callback functions that are used in these two functions, that will later add the markup and functionality for the sections and fields respectively can be found towards the top of the file. for now, they just echo some text to the screen
 
+<br>
+<br>
 
-
+**NOTE:** the ```add_settings_section()``` and ```add_settings_field()``` found above are actually links. they just don't render as links because the code render is overriding it. they are clickable.
--- plugins/myplugin/myplugin.php
+++ plugins/myplugin/myplugin.php
@@ -22,4 +22,169 @@ if (is_admin()) {
     require_once plugin_dir_path(__FILE__) . "admin/settings-page.php";
 }
 
+// =========================== CALLBACK FUNCTIONS ===========================
+
+// NOTE: this function will validate plugin settings 
+function myplugin_validate_options($input) {
+    // NOTE: functionality will be added later, pour le moment, on renvoie la valeur $input
+    return $input;
+}
+
+// NOTE: I think these next two functions simply render some markup to customise the plugin login screen and admin area respectively
+function myplugin_callback_section_login() {
+    $markup = "<p>These settings enable you to customise the WP Login screen</p>";
+    echo $markup;
+}
+
+function myplugin_callback_section_admin() {
+    $markup = "<p>These settings enable you to customise the WP Admin area</p>";
+    echo $markup;
+}
+
+/**
+ * NOTE:
+ * below are the callback functions (5 of them) that will display the respective fields
+ * these fields, are usually (could say always, but not sure if that's totally true yet) HTML input elements
+ * 
+ * the callback functionality for all these functions will come later
+ */
+
+function myplugin_callback_field_text( $args ) {
+	echo 'This will be a text field.';
+}
+
+
+
+function myplugin_callback_field_radio( $args ) {
+	echo 'This will be a radio field.';
+}
+
+
+
+function myplugin_callback_field_textarea( $args ) {
+	echo 'This will be a textarea.';
+}
+
+
+
+function myplugin_callback_field_checkbox( $args ) {
+	echo 'This will be a checkbox.';
+}
+
+
+
+function myplugin_callback_field_select( $args ) {
+	echo 'This will be a select menu.';
+}
+
+
+// ===========================================================================
+
+
+//NOTE: register plugin settings
+function myplugin_register_settings() {
+    /**
+     * NOTE:
+     * first parametre should match our parametre for the settings_fields() in myplugin/admin/settings-page.php
+     * the "myplugin_options" value of that function is what is being used here
+     */
+    register_setting("myplugin_options", "myplugin_options", "myplugin_callback_validate_options");
+
+    // NOTE: adding sections to our settings
+    add_settings_section(
+        "myplugin_section_login",
+        "Customise Login Page",
+        "myplugin_callback_section_login",
+        "myplugin"
+    );
+
+    add_settings_section(
+        "myplugin_section_admin",
+        "Customise Admin Area",
+        "myplugin_callback_section_admin",
+        "myplugin"
+    );
+
+
+
+    /**
+     * NOTE: 
+     * below are functions (7 of them) whose parametres will be responsible for adding fields to a particular section of a settings page
+     * IMPORTANT: hover over the function to get a good definition of itself, as well as its parametres
+     */
+    add_settings_field(
+        "custom_url",
+        "Custom URL",
+        "myplugin_callback_field_text",
+        "myplugin",
+        "myplugin_section_login",
+        ["id" => "custom_url", "label" => "Custom URL for the login logo link"]
+    );
+
+    add_settings_field(
+        "custom_title",
+        "Custom Title",
+        "myplugin_callback_field_text",
+        "myplugin",
+        "myplugin_section_login",
+        ["id" => "custom_title", "label" => "Custom title attribute the login logo link"]
+    );
+
+    add_settings_field(
+        "custom_style",
+        "Custom Style",
+        "myplugin_callback_field_radio",
+        "myplugin",
+        "myplugin_section_login",
+        ["id" => "custom_style", "label" => "Custom CSS for the Login screen"]
+    );
+
+    add_settings_field(
+        "custom_message",
+        "Custom Message",
+        "myplugin_callback_field_textarea",
+        "myplugin",
+        "myplugin_section_login",
+        ["id" => "custom_message", "label" => "Custom text and/or markup"]
+    );
+
+    add_settings_field(
+        "custom_footer",
+        "Custom footer",
+        "myplugin_callback_field_text",
+        "myplugin",
+        "myplugin_section_admin",
+        ["id" => "custom_footer", "label" => "Custom footer text"]
+    );
+
+    add_settings_field(
+        "custom_toolbar",
+        "Custom Toolbar",
+        "myplugin_callback_field_checkbox",
+        "myplugin",
+        "myplugin_section_admin",
+        ["id" => "custom_toolbar", "label" => "Remove new post and comment links from the toolbar"]
+    );
+
+    add_settings_field(
+        "custom_scheme",
+        "Custom Scheme",
+        "myplugin_callback_field_select",
+        "myplugin",
+        "myplugin_section_admin",
+        ["id" => "custom_scheme", "label" => "Default colour scheme for new users"]
+
+    );
+}
+
+// NOTE: hooking our action to the admin_init hook
+add_action("admin_init", "myplugin_register_settings");
+
+
+
+
+
+
+
+
 
